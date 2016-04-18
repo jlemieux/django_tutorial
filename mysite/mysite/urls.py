c3b332bd3287ae852d036235a241fed57de546b0
James Lemieux <james.lemieux@sau20.org>
2016-04-18 14:42:00 -0400

    not sure, just revisited this project

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from polls import views
 
 urlpatterns = [
-    url(r'^$', views.home),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ]
